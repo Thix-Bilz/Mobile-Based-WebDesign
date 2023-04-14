c6b7ee229feb23d1fae2f718ed54ce1648c006d4
Bilal Autangzeb Khan <b20f0308se016@fecid.paf-iast.edu.pk>
2023-04-13 23:31:21 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Navbar
+(function () {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
+
+//Sidebar
